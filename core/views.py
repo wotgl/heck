1fafa950b808f3d9f37466526d6b72180846497c
Sasha Konyshev <sasha.konyshev1003@gmail.com>
2016-11-27 04:39:27 +0300

    hehe

--- core/views.py
+++ core/views.py
@@ -124,7 +124,7 @@ def redirect(request):
 
     url = 'https://oauth.vk.com/access_token?client_id=5748766&client_secret=TKCRZDIecDW5F3TKy6yY&redirect_uri=https://reunited.tk/api/redirect/?group_id=%s&code=%s' % (group_id, code)
     r = requests.get(url)
-    # print r.text
+    print r.text
     token = json.loads(r.text)['access_token_' + group_id]
     print token
     community = Community.objects.create(vk_id=group_id, token=token)
