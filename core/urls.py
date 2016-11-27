3a74082c6a16637cf5ba6c2e1edd7790d9a89ce6
Sasha Konyshev <sasha.konyshev1003@gmail.com>
2016-11-27 04:12:13 +0300

    hehe

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^set_access_token/$', views.set_access_token),
     url(r'^get_comments/$', views.get_comments),
     url(r'^redirect/$', views.redirect),
-    url(r'^get_comments_admin/$', views.get_comments_admin)
-    url(r'^ban_user/$', views.ban_user)
+    url(r'^get_comments_admin/$', views.get_comments_admin),
+    url(r'^ban_user/$', views.ban_user),
 ]
