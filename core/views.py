62edd9b745c452ecc6c77985a29d24af09e12891
Sasha Konyshev <sasha.konyshev1003@gmail.com>
2016-11-27 05:52:38 +0300

    hehe

--- core/views.py
+++ core/views.py
@@ -168,7 +168,7 @@ def get_comments_admin(request):
         print url
         r = requests.get(url)
         for k in json.loads(r.text)['response']:
-            avatars[k['uid']] = k['photo_50']
+            avatars[k['uid']] = k['photo_100']
         
         for k in cm:
             result.append({'id': k.cid, 'text': k.text, 'score': random.random(), 'uid': k.user.uid, 'pid': k.post.pid, 'photo': avatars[int(k.user.uid)]})
